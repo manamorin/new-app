73b7e8900218f0fbf96da8c92aef23959353ad9d
manamorin <amame95chihiron@gmail.com>
2014-12-05 15:41:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var milkcocoa = new MilkCocoa("https://io-dummydummy.mlkcca.com/");
+var milkcocoa = new ilkCocoa("https://io-dummydummy.mlkcca.com/");
 /* your-app-id にアプリ作成時に発行される"io-"から始まるapp-idを記入します */
 var chatDataStore = milkcocoa.dataStore("chat");
 var textArea, board;
